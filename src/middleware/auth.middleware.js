8277f8083a1bc19bc253f609af260d8cc5cc475d
Mauricio Patiño <patto971120@gmail.com>
2022-12-08 16:32:12 -0500

    entregable

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,29 @@
+const passport = require('passport')
+
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+
+const { findUserById } = require("../users/users.controllers")
+
+const options = {
+    jwtFromRequest : ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+    secretOrKey : 'asd' 
+}
+
+passport.use(
+    new JwtStrategy(options, async (tokenDecoded, done) => {
+        try {
+            const user = await findUserById(tokenDecoded.id)
+            if(user){
+                return done(null, tokenDecoded)
+            } else {
+                return done(null, false)
+            }
+        } catch (error) {
+            return done(error, false)
+        }   
+    })
+)
+
+
+module.exports = passport
